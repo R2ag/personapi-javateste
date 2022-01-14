4488e1f86ee1ef0cf1fcaf31a88971758be6f2ac
Rafael Lacerda <rlag@outlook.com>
2022-01-14 20:39:08 -0300

    Revisão do README.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# personapi-javateste
-API REST com Spring Boot, desenvolvida para estudos
+# PersonApi - API REST Com Spring Boot Para sistema de gestão de pessoas
+Esta API foi desenvolvida para estudar as tecnologias utilizadas e praticar a programação.
+
+### Até o momento, foram criadas as seguintes funções para a API:
+- Cadastrar Pessoa;
+- Editar uma pessoa cadastrada, identificada por id;
+- Listar uma pessoa especifica que esteja cadastrada, a identificando por id;
+- Listar todas as pessoas cadastradas;
+- Excluir uma pessoa especifica, identificando por id.
+
+
--- src/main/java/com/rlag/personapi/controller/PersonController.java
+++ src/main/java/com/rlag/personapi/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.rlag.personapi.dto.request.PersonDTO;
 import com.rlag.personapi.dto.response.MessageResponseDTO;
 import com.rlag.personapi.exception.PersonNotFoundException;
 import com.rlag.personapi.service.PersonService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +14,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
     private PersonService personService;
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
