0e2f71bcc27a612a8bb1a48059d008a1485fc805
Rafael Lacerda <rlag@outlook.com>
2022-01-13 21:57:28 -0300

    Atualizando dependencias

--- src/main/java/com/rlag/personapi/service/PersonService.java
+++ src/main/java/com/rlag/personapi/service/PersonService.java
@@ -8,23 +8,35 @@ import com.rlag.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
+
         Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Created person with ID "+ savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll(){
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+
     }
 }
---
+++ target/generated-sources/annotations/com/rlag/personapi/mapper/PersonMapperImpl.java
@@ -0,0 +1,55 @@
+package com.rlag.personapi.mapper;
+
+import com.rlag.personapi.dto.request.PersonDTO;
+import com.rlag.personapi.dto.request.PersonDTO.PersonDTOBuilder;
+import com.rlag.personapi.entity.Person;
+import com.rlag.personapi.entity.Person.PersonBuilder;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-01-13T21:53:55-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.13 (Oracle Corporation)"
+)
+public class PersonMapperImpl implements PersonMapper {
+
+    @Override
+    public Person toModel(PersonDTO personDTO) {
+        if ( personDTO == null ) {
+            return null;
+        }
+
+        PersonBuilder person = Person.builder();
+
+        if ( personDTO.getBirthDate() != null ) {
+            person.birthDate( LocalDate.parse( personDTO.getBirthDate(), DateTimeFormatter.ofPattern( "dd-MM-yyyy" ) ) );
+        }
+        person.id( personDTO.getId() );
+        person.firstName( personDTO.getFirstName() );
+        person.lastName( personDTO.getLastName() );
+        person.cpf( personDTO.getCpf() );
+
+        return person.build();
+    }
+
+    @Override
+    public PersonDTO toDTO(Person person) {
+        if ( person == null ) {
+            return null;
+        }
+
+        PersonDTOBuilder personDTO = PersonDTO.builder();
+
+        personDTO.id( person.getId() );
+        personDTO.firstName( person.getFirstName() );
+        personDTO.lastName( person.getLastName() );
+        personDTO.cpf( person.getCpf() );
+        if ( person.getBirthDate() != null ) {
+            personDTO.birthDate( DateTimeFormatter.ISO_LOCAL_DATE.format( person.getBirthDate() ) );
+        }
+
+        return personDTO.build();
+    }
+}
